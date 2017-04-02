3ad6c7bff79fa5cb98c2ddf1ab78e0fbca3f5e1d
user125303 <user125303@ny093.local>
2017-04-02 10:56:53 -0400

    TITLES ADDED

--- Pitonew IOS/ViewController.swift
+++ Pitonew IOS/ViewController.swift
@@ -71,6 +71,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var O2TextField: UITextField!
     //Area 
     
+    @IBOutlet weak var widthTitle: UILabel!
+    @IBOutlet weak var velocityAverageTitle: UILabel!
+    @IBOutlet weak var normalAirFlowTitle: UILabel!
+    @IBOutlet weak var dryBulbTitle: UILabel!
+    @IBOutlet weak var atmoshpericPressureTItle: UILabel!
+    @IBOutlet weak var massAirFlowTitle: UILabel!
+    @IBOutlet weak var actualAirFlowTitle: UILabel!
+    @IBOutlet weak var gasDensityTitle: UILabel!
+    @IBOutlet weak var dynamicVelocityTitle: UILabel!
+    
+    @IBOutlet weak var ductPressureTitle: UILabel!
+    
+    
     func average(nums: [Double]) -> Double {
         
         var total = 0.0
@@ -270,13 +283,319 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func toolbarClearButton(_ sender: Any) {
+        widthTextField.text="1"
+        heightTextField.text="1"
+        molecularWeightTextField.text="28.96"
+        dryBulbTemperatureTextField.text="1"
+        staticPressureTextField.text="1"
+        elevationAboveSeaLevelTextField.text="1"
+        wetBulbTemperatureTextField.text="1"
+        pressureAtSeaLevelTextField.text="1"
+        pitotTubeCoeffecientTextField.text="1"
+        CO2TextField.text="0.03"
+        ArTextField.text="20.95"
+        N2TextField.text="78.09"
+        O2TextField.text="0.93"
+        H20TextField.text="0.0"
+        
+        
+        
+        self.widthUnitLabel.isHidden = false
+        self.heightUnitLabel.isHidden = false
+        self.dryBulbUnitLabel.isHidden = false
+        self.staticPressureUnitLabel.isHidden = false
+        self.pressureSeaLevelUnitLabel.isHidden = false
+        self.wetBulbUnitLabel.isHidden = false
+        
+        self.areaUnitLabel.isHidden = true
+        self.averageVelocityUnitLabel.isHidden = true
+        self.normalAirFlowUnitLabel.isHidden = true
+        self.atmosphericPressureUnitLabel.isHidden = true
+        self.actualAirFlowUnitLabel.isHidden = true
+        self.massAirFlowUnitLabel.isHidden = true
+        self.ductPressureUnitLabel.isHidden = true
+        self.gasDensityUnitLabel.isHidden = true
+        
+        self.averageVelocityResultLabel.isHidden = true
+        self.areaResultLabel.isHidden = true
+        self.normalAirFlowResultLabel.isHidden = true
+        self.gasDensityResultLabel.isHidden = true
+        self.ductPressureResultLabel.isHidden = true
+        self.actualAirFlowResultLabel.isHidden = true
+        self.massAirFlowResultLabel.isHidden = true
+        self.atmosphericPressureResultLabel.isHidden = true
+        self.relativeHumidityResultLabel.isHidden = true
+        
+        
+        
+        self.AirCompositionSwitch.isHidden = false
+        self.wetBulbSwitch.isHidden = false
+        //self.pipeShapeSwitch.isHidden = false
+        self.CO2Label.isHidden = false
+        
+        
+        self.widthTextField.isHidden = false
+        self.heightTextField.isHidden = false
+        self.molecularWeightTextField.isHidden = false
+        self.dryBulbTemperatureTextField.isHidden = false
+        self.staticPressureTextField.isHidden = false
+        self.elevationAboveSeaLevelTextField.isHidden = false
+        self.wetBulbTemperatureTextField.isHidden = false
+        self.pitotTubeCoeffecientTextField.isHidden = false
+        self.pressureAtSeaLevelTextField.isHidden = false
+        
+        self.widthTitle: UILabel!
+        self.velocityAverageTitle: UILabel!
+        self.normalAirFlowTitle: UILabel!
+        self.dryBulbTitle: UILabel!
+        self.atmoshpericPressureTItle: UILabel!
+        self.massAirFlowTitle: UILabel!
+        @IBOutlet weak var actualAirFlowTitle: UILabel!
+        @IBOutlet weak var gasDensityTitle: UILabel!
+        @IBOutlet weak var dynamicVelocityTitle: UILabel!
+        
+        @IBOutlet weak var ductPressureTitle: UILabel!
+        
+    }
     
+    @IBAction func toolbarCalculateButton(_ sender: Any) {
+        
+        if((widthTextField.text?.isEmpty)! || (heightTextField.text?.isEmpty)! || (pitotTubeCoeffecientTextField.text?.isEmpty)! || (staticPressureTextField.text?.isEmpty)! || (dryBulbTemperatureTextField.text?.isEmpty)! || (elevationAboveSeaLevelTextField.text?.isEmpty)! || (wetBulbTemperatureTextField.text?.isEmpty)! || (pressureAtSeaLevelTextField.text?.isEmpty)! || (molecularWeightTextField.text?.isEmpty)!) //|| (CO2TextField.text?.isEmpty)! || (O2TextField.text?.isEmpty)! || (N2TextField.text?.isEmpty)! || (ArTextField.text?.isEmpty)! || (H20TextField.text?.isEmpty)!)
+        {
+            
+        }
+        else{
+            var dynamicPressureArray:[Double] = [1]
+            let width = Double(widthTextField.text!)
+            let height = Double(heightTextField.text!)
+            let pilotTubeCoeffecient = Double(pitotTubeCoeffecientTextField.text!)
+            let staticPressure = Double(staticPressureTextField.text!)
+            let dryBulbTemperature = Double(dryBulbTemperatureTextField.text!)
+            let elevationAboveSealevel = Double(elevationAboveSeaLevelTextField.text!)
+            let wetBulbTemperature = Double(wetBulbTemperatureTextField.text!)
+            let seaLevelPressure = Double(pressureAtSeaLevelTextField.text!)
+            var molecularWeight = Double(molecularWeightTextField.text!)
+            let C02Composition = Double(CO2TextField.text!)
+            let O2Composition = Double(O2TextField.text!)
+            let N2Composition = Double(N2TextField.text!)
+            let ARComposition = Double(ArTextField.text!)
+            let H2OComposition = Double(H20TextField.text!)
+            var relativeHumidity: Double
+            var dryBulbRankine: Double
+            var wetBulbRankine: Double
+            var Kd: Double
+            var humidityH20WetAir = 0.0
+            var Kw: Double
+            var dryMolecularWeight: Double
+            var partialPressureOfWaterPA: Double
+            var dryBulbWaterSaturationPressurePD: Double
+            var wetBulbWaterSaturationPressurePW: Double
+            var partialWaterPressureDueToDepressionPM: Double
+            let standardAirMolarWeight = 28.96;
+            let criticalPressureH20 = 166818.0;
+            let criticalTemperatureH20 = 1165.67;
+            let pressMmHg=754.30;
+            var humidityH20DryAir:Double
+            
+            if(wetBulbSwitch.isOn){
+                if(UnitSwitch.isOn){
+                    dryBulbRankine = (dryBulbTemperature! * 1.8 + 32)  + 459.67
+                    wetBulbRankine = (wetBulbTemperature! * 1.8 + 32) + 459.67
+                }
+                else {
+                    dryBulbRankine = dryBulbTemperature! + 459.67
+                    wetBulbRankine = wetBulbTemperature! + 459.67
+                }
+                
+                Kd = -0.0000000008833 * pow(dryBulbRankine,3) + 0.000003072 * pow(dryBulbRankine,2) - 0.003469 * dryBulbRankine + 4.39553
+                Kw = -0.0000000008833 * pow(wetBulbRankine,3)+0.000003072 * pow(wetBulbRankine,2) - 0.003469 * wetBulbRankine + 4.39553
+                dryBulbWaterSaturationPressurePD = criticalPressureH20 * pow(10, Kd * (1 - (criticalTemperatureH20 / dryBulbRankine)))
+                wetBulbWaterSaturationPressurePW = criticalPressureH20 * pow(10, Kw * (1 - (criticalTemperatureH20 / wetBulbRankine)))
+                partialWaterPressureDueToDepressionPM = 0.000367 * (1 + ((wetBulbRankine-459.67) - 32) / 1571) * (pressMmHg - wetBulbWaterSaturationPressurePW) * ((dryBulbRankine - 459.67) - (wetBulbRankine - 459.67))
+                
+                // if((wetBulbWaterSaturationPressurePW - partialWaterPressureDueToDepressionPM) / dryBulbWaterSaturationPressurePD> = 100 || (wetBulbWaterSaturationPressurePW -  partialWaterPressureDueToDepressionPM) / dryBulbWaterSaturationPressurePD < 0){
+                //}
+                // else{
+                
+                relativeHumidity = 100 * (wetBulbWaterSaturationPressurePW-partialWaterPressureDueToDepressionPM)/dryBulbWaterSaturationPressurePD
+                // }
+                
+                partialPressureOfWaterPA = 0.01 * relativeHumidity * dryBulbWaterSaturationPressurePD
+                
+                if(wetBulbSwitch.isOn){
+                    humidityH20WetAir = partialPressureOfWaterPA / pressMmHg
+                }
+                else{
+                    humidityH20WetAir = 0
+                }
+                
+                var part1 = 44.01 * (C02Composition! * (1 - humidityH20WetAir))
+                var part2 = 31.999 * (O2Composition! * ( 1 - humidityH20WetAir))
+                var part3 = 28.013*(N2Composition! * (1-humidityH20WetAir))
+                var part4 = 39.948*(ARComposition! * (1 - humidityH20WetAir))
+                
+                dryMolecularWeight = (part1 + part2 + part3 + part4)/100;
+                
+                humidityH20DryAir = (18.02 / dryMolecularWeight) * (partialPressureOfWaterPA / (pressMmHg - partialPressureOfWaterPA))
+                
+            }
+            else{
+                
+            }
+            
+            
+            if(AirCompositionSwitch.isOn){
+                if(wetBulbSwitch.isOn){
+                    var part1 = 0.03 * (1 - humidityH20WetAir)
+                    var part2 = 20.95 * (1 - humidityH20WetAir)
+                    var part3 = 78.09 * (1 - humidityH20WetAir)
+                    var part4 = 0.93 * (1 - humidityH20WetAir)
+                    var part5 = 100 * humidityH20WetAir
+                    
+                    
+                    molecularWeight = part1 + part2 + part3 + part4 + part5
+                }
+                else {
+                    molecularWeight = 28.96
+                }
+            }
+            else {
+                if(wetBulbSwitch.isOn){
+                    var part1 = 44.01 * C02Composition! * (1 - humidityH20WetAir)
+                    var part2 = 31.999 * O2Composition! * (1 - humidityH20WetAir)
+                    var part3 = 28.013 * N2Composition! * (1 - humidityH20WetAir)
+                    var part4 = 39.948 * ARComposition! * (1 - humidityH20WetAir)
+                    var part5 = 18.016 * 100 * humidityH20WetAir
+                    molecularWeight = (part1 + part2 + part3 + part4 + part5)/100
+                }
+                else {
+                    var part1 = 44.01 * C02Composition!
+                    var part2 = 31.999 * O2Composition!
+                    var part3 = 28.013 * N2Composition!
+                    var part4 = 39.948 * ARComposition!
+                    molecularWeight = (part1 + part2 + part3 + part4) / 100
+                }
+                
+            }
+            /*
+             
+             
+             
+             
+             
+             }
+             else {
+             }
+             
+             }
+             
+             */
+            
+            
+            
+            let area = width!*height!
+            let atmosphericPressure = seaLevelPressure!*pow(10, -0.00001696*elevationAboveSealevel!);
+            let ductPressure = atmosphericPressure + staticPressure!*0.249088
+            let gasDensity = 1000 * ductPressure / (273.15 + dryBulbTemperature!) / (8314.3 / molecularWeight!)
+            
+            var dynamicVelocityArray = [Double]()
+            for item in dynamicPressureArray {
+                dynamicVelocityArray.append(pilotTubeCoeffecient!*pow(2*item*1000/4.01864/gasDensity,0.5))
+            }
+            let averageVelocity = average(nums: dynamicVelocityArray)
+            let actualAirFlow = averageVelocity*area*3600
+            let massAirFlow=actualAirFlow*gasDensity/3600;
+            let normalAirFlow = actualAirFlow*ductPressure/101.325*273.15/(273.15+((dryBulbTemperature!-32)/1.8))
+            
+            
+            
+            
+            let numberTwoDigitsFomatter: NumberFormatter = {
+                let nf = NumberFormatter()
+                nf.numberStyle = .decimal
+                nf.minimumFractionDigits = 2
+                nf.maximumFractionDigits = 2
+                return nf
+            }()
+            
+            let numberThreeDigitsFomatter: NumberFormatter = {
+                let nf = NumberFormatter()
+                nf.numberStyle = .decimal
+                nf.minimumFractionDigits = 3
+                nf.maximumFractionDigits = 3
+                return nf
+            }()
+            
+            
+            let numberFourDigitsFomatter: NumberFormatter = {
+                let nf = NumberFormatter()
+                nf.numberStyle = .decimal
+                nf.minimumFractionDigits = 4
+                nf.maximumFractionDigits = 4
+                return nf
+            }()
+            areaResultLabel.text =  numberThreeDigitsFomatter.string(from: area as NSNumber)
+            averageVelocityResultLabel.text = numberTwoDigitsFomatter.string(from: averageVelocity as NSNumber)
+            normalAirFlowResultLabel.text = String(Int(normalAirFlow))
+            atmosphericPressureResultLabel.text = numberTwoDigitsFomatter.string(from: atmosphericPressure as NSNumber)
+            massAirFlowResultLabel.text = numberTwoDigitsFomatter.string(from: massAirFlow as NSNumber)
+            actualAirFlowResultLabel.text = String(Int(actualAirFlow))
+            ductPressureResultLabel.text = numberTwoDigitsFomatter.string(from: ductPressure as NSNumber)
+            gasDensityResultLabel.text = numberFourDigitsFomatter.string(from: gasDensity as NSNumber)
+            
+            self.widthUnitLabel.isHidden = true
+            self.heightUnitLabel.isHidden = true
+            self.dryBulbUnitLabel.isHidden = true
+            self.staticPressureUnitLabel.isHidden = true
+            self.pressureSeaLevelUnitLabel.isHidden = true
+            self.wetBulbUnitLabel.isHidden = true
+            
+            self.areaUnitLabel.isHidden = false
+            self.averageVelocityUnitLabel.isHidden = false
+            self.normalAirFlowUnitLabel.isHidden = false
+            self.atmosphericPressureUnitLabel.isHidden = false
+            self.actualAirFlowUnitLabel.isHidden = false
+            self.massAirFlowUnitLabel.isHidden = false
+            self.ductPressureUnitLabel.isHidden = false
+            self.gasDensityUnitLabel.isHidden = false
+            
+            self.averageVelocityResultLabel.isHidden = false
+            self.areaResultLabel.isHidden = false
+            self.normalAirFlowResultLabel.isHidden = false
+            self.gasDensityResultLabel.isHidden = false
+            self.ductPressureResultLabel.isHidden = false
+            self.actualAirFlowResultLabel.isHidden = false
+            self.massAirFlowResultLabel.isHidden = false
+            self.atmosphericPressureResultLabel.isHidden = false
+            self.relativeHumidityResultLabel.isHidden = false
+            
+            
+            
+            self.AirCompositionSwitch.isHidden = true
+            self.wetBulbSwitch.isHidden = true
+            //     self.pipeShapeSwitch.isHidden = true
+            self.CO2Label.isHidden = true
+            
+            
+            self.widthTextField.isHidden = true
+            self.heightTextField.isHidden = true
+            self.molecularWeightTextField.isHidden = true
+            self.dryBulbTemperatureTextField.isHidden = true
+            self.staticPressureTextField.isHidden = true
+            self.elevationAboveSeaLevelTextField.isHidden = true
+            self.wetBulbTemperatureTextField.isHidden = true
+            self.pitotTubeCoeffecientTextField.isHidden = true
+            self.pressureAtSeaLevelTextField.isHidden = true        }
+        
+        
+    }
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBAction func segmentedControlindexChange(_ sender: Any) {
        
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-            {
+            
                 self.widthUnitLabel.isHidden = false
                 self.heightUnitLabel.isHidden = false
                 self.dryBulbUnitLabel.isHidden = false
@@ -307,7 +626,7 @@ class ViewController: UIViewController {
                 
                 self.AirCompositionSwitch.isHidden = false
                 self.wetBulbSwitch.isHidden = false
-                self.pipeShapeSwitch.isHidden = false
+                //self.pipeShapeSwitch.isHidden = false
                 self.CO2Label.isHidden = false
   
                 
@@ -324,10 +643,10 @@ class ViewController: UIViewController {
                 
    
    
-            }
+            
             
         case 1:
-            {
+            
         
                 self.widthUnitLabel.isHidden = true
                 self.heightUnitLabel.isHidden = true
@@ -359,7 +678,7 @@ class ViewController: UIViewController {
                 
                 self.AirCompositionSwitch.isHidden = true
                 self.wetBulbSwitch.isHidden = true
-                self.pipeShapeSwitch.isHidden = true
+           //     self.pipeShapeSwitch.isHidden = true
                 self.CO2Label.isHidden = true
   
                 
@@ -373,12 +692,12 @@ class ViewController: UIViewController {
                 self.pitotTubeCoeffecientTextField.isHidden = true
                 self.pressureAtSeaLevelTextField.isHidden = true
                 
-        }
+        
             
         default:
             break
+        
         }
-
     }
     
     @IBAction func unitSwitchClicked(_ sender: Any) {
